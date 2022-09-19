b6db4a6f8b7e8f1a21475e6760e9c7048d54e592
John Dinh <johnduydinh@gmail.com>
2022-09-19 12:17:06 -0400

    add missing step for nvidia drivers

--- ubuntu-server-setup.sh
+++ ubuntu-server-setup.sh
@@ -5,8 +5,10 @@ sudo apt -y upgrade
 sudo apt-get install linux-headers-$(uname -r)
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt update
 sudo apt -y install cuda-drivers
+rm cuda-keyring_1.0-1_all.deb
 
 #Nvidia-Docker (NVIDIA Container Toolkit)
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
