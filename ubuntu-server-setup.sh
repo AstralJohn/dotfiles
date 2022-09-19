ae1fd89bed5b67789a174bc7c69269054890a32d
John Dinh <johnduydinh@gmail.com>
2022-09-19 12:10:20 -0400

    fix portainer startup permission

--- ubuntu-server-setup.sh
+++ ubuntu-server-setup.sh
@@ -55,4 +55,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
 
 # Setup and start Portainer
 sudo docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
